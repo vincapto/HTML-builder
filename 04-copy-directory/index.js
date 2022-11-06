c4d5a2e22ec2b577506bcd86864640186013a619
Raman <vincapto@gmail.com>
2022-11-06 22:47:00 +0100

    feat: 04-copy-directory solved

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,45 @@
+const fs = require('fs');
+const path = require('path');
 
+const copyPath = path.resolve(__dirname, 'files-copy');
+
+async function start() {
+  try {
+    await fs.promises.rm(copyPath, { recursive: true, force: true }, (err) => {
+      if (err) console.log('folder not exist');
+      console.log('clear project dir');
+    });
+    await fs.promises.mkdir(copyPath, { recursive: true });
+    await readFolder(path.resolve(__dirname, 'files'));
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+start();
+
+async function readFiles(dirname, files) {
+  console.log('DIR', dirname);
+  files.forEach((file, key) => {
+    const { base } = path.parse(file.name);
+    const filePath = path.resolve(dirname, base);
+    fs.stat(filePath, (error, stats) => {
+      if (error) {
+        console.log(error);
+      } else {
+        if (stats.isFile())
+          fs.promises.copyFile(
+            path.join(dirname, base),
+            path.join(copyPath, base)
+          );
+      }
+    });
+  });
+}
+
+async function readFolder(dirname) {
+  await fs.readdir(dirname, { withFileTypes: true }, (err, files) => {
+    if (err) console.log(err);
+    else readFiles(dirname, files);
+  });
+}
